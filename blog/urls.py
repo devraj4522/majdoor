3bb9fb0f67bc04cb41930758873e01caf664eeae
Dev Raj <rehansingh.4522@gmail.com>
2020-01-22 21:19:00 +0530

    "Currently working alone"

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name ="Blog Home"),
+    path('', views.index, name="Blog Home"),
+    path('about/', views.about, name="About"),
+    path('services/', views.services, name="Services"),
+    path('contact/', views.contact, name="Contact"),
+    path('login/', views.login, name="Login"),
+    path('register/', views.contact, name="Register"),
+    path('menu/', views.contact, name="Menu"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,3 +4,21 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'blog/index.html')
+
+def about(request):
+    return render(request, 'blog/about.html')
+
+def services(request):
+    return render(request, 'blog/services.html')
+
+def contact(request):
+    return render(request, 'blog/contact.html')
+
+def login(request):
+    return render(request, 'blog/login.html')
+
+def register(request):
+    return render(request, 'blog/register.html')
+
+def menu(request):
+    return render(request, 'blog/menu.html')
